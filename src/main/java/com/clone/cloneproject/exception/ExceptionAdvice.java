08e0e8ff0320b7069a751b6b0731e3df972c2fa1
GYUMIN KWON <rbals7398@naver.com>
2022-04-18 22:17:08 +0900

    회원가입 유효성 전역 예외처리

--- src/main/java/com/clone/cloneproject/exception/ExceptionAdvice.java
+++ src/main/java/com/clone/cloneproject/exception/ExceptionAdvice.java
@@ -7,8 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -24,13 +29,19 @@ public class ExceptionAdvice {
         return new ResponseEntity(new ExceptionDto(exception.getExceptionType().getErrorCode()),exception.getExceptionType().getHttpStatus());
     }
 
-    //@Valid 에서 예외 발생
-    @ExceptionHandler(BindException.class)
-    public ResponseEntity handleValidEx(BindException exception){
 
-        log.error("@ValidException 발생! {}", exception.getMessage() );
-        return new ResponseEntity(new ExceptionDto(2000),HttpStatus.BAD_REQUEST);
+    //회원가입 Valid 유효성 검사
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>>handleValidationExceptions(
+            MethodArgumentNotValidException ex){
+
+        Map<String,String> erros = new HashMap<>();
+        ex.getBindingResult().getAllErrors()
+                .forEach(c -> erros.put(((FieldError) c).getField(), c.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(erros);
+
     }
+
 
     //HttpMessageNotReadableException  => json 파싱 오류
     @ExceptionHandler(HttpMessageNotReadableException.class)
