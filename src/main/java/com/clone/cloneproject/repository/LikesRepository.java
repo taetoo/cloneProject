f93ea52ffc9738178c6769ac2d52a3904eed15a5
GYUMIN KWON <rbals7398@naver.com>
2022-04-20 19:04:12 +0900

    좋아요 삭제 기능 재수정(2)

--- src/main/java/com/clone/cloneproject/repository/LikesRepository.java
+++ src/main/java/com/clone/cloneproject/repository/LikesRepository.java
@@ -2,6 +2,7 @@ package com.clone.cloneproject.repository;
 
 
 import com.clone.cloneproject.domain.Likes;
+import com.clone.cloneproject.domain.Posts;
 import com.clone.cloneproject.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,8 @@ public interface LikesRepository extends JpaRepository<Likes,Long> {
 
 
     Optional<Likes> findLikesByUserAndPostsId(User user, Long id);
-    void deleteLikesByUser(User user);
+    void deleteLikesByUserAndPosts(User user, Posts posts);
+
+
 
 }
